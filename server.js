dbb8a8b7305ea3e8790748d8114e27fd629e5511
Jaron <jaron@hmcnetwork.nl>
2019-12-18 15:31:21 +0100

    Created route

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/subscribers')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const citiesRouter = require('./routes/cities.js')
+app.use('/cities', citiesRouter)
 
 app.listen(3000, () => console.log('Server started'))
